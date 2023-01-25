d0baaca8154293091cfccfaa1ca345b1d7ae4c7b
Ruben d'Arco <cyclops-github@prof-x.net>
2023-01-25 20:59:56 +0000

    Updates and making stuff work.
    - two interrupts

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ The RAK1904 module has the LIS3DH accelerometer on it.
 Info page: https://www.st.com/en/mems-and-sensors/lis3dh.html
 DataSheet: https://www.st.com/resource/en/datasheet/lis3dh.pdf
 
-
+Info on the INT1_THS/INT2_THS configuration: https://community.st.com/s/question/0D50X00009fFFXzSAO/how-is-the-int1ths-scaled-on-the-lis3dh-sensor (in short, it's whatever FS is configured times the value of that register)
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,67 @@
 
 LIS3DH Sensor(I2C_MODE, 0x18);
 
-volatile bool g_moved = false;
+volatile bool g_int1_triggered = false;
+volatile bool g_int2_triggered = false;
 
 void lis3dh_read_data()
 {
   // read the sensor value
   Serial.print(" X(g) = ");
-  Serial.println(Sensor.readFloatAccelX(), 4);
+  Serial.print(Sensor.readFloatAccelX(), 4);
   Serial.print(" Y(g) = ");
-  Serial.println(Sensor.readFloatAccelY(), 4);
+  Serial.print(Sensor.readFloatAccelY(), 4);
   Serial.print(" Z(g)= ");
   Serial.println(Sensor.readFloatAccelZ(), 4);
 }
 
-void pin_high()
+void describe_src_int(uint8_t src)
 {
-  g_moved = true;
+  if ((src & LIS3DHEnums::INT_SRC::IA) == LIS3DHEnums::INT_SRC::IA)
+  {
+    Serial.print("Interrupt active on: ");
+  }
+  if ((src & LIS3DHEnums::INT_SRC::ZH) == LIS3DHEnums::INT_SRC::ZH)
+  {
+    Serial.print("ZH ");
+  }
+
+  if ((src & LIS3DHEnums::INT_SRC::ZL) == LIS3DHEnums::INT_SRC::ZL)
+  {
+    Serial.print("ZL ");
+  }
+
+  if ((src & LIS3DHEnums::INT_SRC::YH) == LIS3DHEnums::INT_SRC::YH)
+  {
+    Serial.print("YH ");
+  }
+
+  if ((src & LIS3DHEnums::INT_SRC::YL) == LIS3DHEnums::INT_SRC::YL)
+  {
+    Serial.print("YL ");
+  }
+
+  if ((src & LIS3DHEnums::INT_SRC::XH) == LIS3DHEnums::INT_SRC::XH)
+  {
+    Serial.print("XH ");
+  }
+
+  if ((src & LIS3DHEnums::INT_SRC::XL) == LIS3DHEnums::INT_SRC::XL)
+  {
+    Serial.print("XL ");
+  }
+
+  Serial.println();
+}
+
+void int1_trigger()
+{
+  g_int1_triggered = true;
+}
+
+void int2_trigger()
+{
+  g_int2_triggered = true;
 }
 
 void setup()
@@ -45,6 +90,8 @@ void setup()
   Wire.begin();
   delay(1000);
 
+  Sensor.settings.tempEnabled = 0;
+  Sensor.settings.adcEnabled = 0;
   if (Sensor.begin() != 0)
   {
     Serial.println("Problem starting the sensor at 0x18.");
@@ -54,50 +101,62 @@ void setup()
     Serial.println("Sensor at 0x18 started.");
   }
 
-  // // Set low power mode
-  // uint8_t data_to_write = 0;
-  // Sensor.readRegister(&data_to_write, LIS3DH_CTRL_REG1);
-  // data_to_write |= 0x08;
-  // Sensor.writeRegister(LIS3DH_CTRL_REG1, data_to_write);
-  // delay(100);
-
-  // data_to_write = 0;
-  // Sensor.readRegister(&data_to_write, 0x1E);
-  // data_to_write |= 0x90;
-  // Sensor.writeRegister(0x1E, data_to_write);
-  // delay(100);
-
   // configurations for control registers
-  Sensor.writeRegister(0x20, 0xA7); // Write A7h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode.
-  Sensor.writeRegister(0x21, 0x09); // Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
-  Sensor.writeRegister(0x22, 0x40); // Write 40h into CTRL_REG3;      // ACC AOI1 interrupt signal is routed to INT1 pin.
-  Sensor.writeRegister(0x23, 0x00); // Write 00h into CTRL_REG4;      // Full Scale = +/-2 g
-  Sensor.writeRegister(0x24, 0x08); // Write 08h into CTRL_REG5;      // Default value is 00 for no latching. Interrupt signals on INT1 pin is not latched.
-                                    // Users don’t need to read the INT1_SRC register to clear the interrupt signal.
-  // configurations for wakeup and motionless detection
-  Sensor.writeRegister(0x32, 0x10); // Write 10h into INT1_THS;          // Threshold (THS) = 16LSBs * 15.625mg/LSB = 250mg.
-  Sensor.writeRegister(0x33, 0x00); // Write 00h into INT1_DURATION;     // Duration = 1LSBs * (1/10Hz) = 0.1s.
-  // readRegister();  //Dummy read to force the HP filter to set reference acceleration/tilt value
-  Sensor.writeRegister(0x30, 0x2A); // Write 2Ah into INT1_CFG;          // Enable XLIE, YLIE, ZLIE interrupt generation, OR logic.
+  // Sensor.writeRegister(LIS3DH_TEMP_CFG_REG, 0x00);
+  Sensor.writeRegister(LIS3DH_CTRL_REG1, 0); // power it down.
+  Sensor.writeRegister(LIS3DH_CTRL_REG0, LIS3DHEnums::CTRL_REG0::PullUpConnected);
+  delay(2000);
+  Sensor.writeRegister(LIS3DH_CTRL_REG1, (LIS3DHEnums::CTRL_REG1::ORD0 | LIS3DHEnums::CTRL_REG1::LPen | LIS3DHEnums::CTRL_REG1::Xen | LIS3DHEnums::CTRL_REG1::Yen));
+  Sensor.writeRegister(LIS3DH_CTRL_REG2, (LIS3DHEnums::CTRL_REG2::HP_IA2 | LIS3DHEnums::CTRL_REG2::HP_IA1));
+  Sensor.writeRegister(LIS3DH_CTRL_REG3, (LIS3DHEnums::CTRL_REG3::I1_IA1));
+  Sensor.writeRegister(LIS3DH_CTRL_REG4, LIS3DHEnums::CTRL_REG4::FS_2G);
+  Sensor.writeRegister(LIS3DH_CTRL_REG5, 0);
+  Sensor.writeRegister(LIS3DH_CTRL_REG6, ( LIS3DHEnums::CTRL_REG6::I2_IA2));
+  Sensor.writeRegister(LIS3DH_REFERENCE, 0);
+
+  // uint8_t dummy = 0;
+  // Sensor.readRegister(&dummy, LIS3DH_REFERENCE);
+  Sensor.writeRegister(LIS3DH_INT1_THS, 0x04);      // Threshold (THS) = 0000 0001 = 1 LSBs * 15.625mg = 16mg
+  Sensor.writeRegister(LIS3DH_INT1_DURATION, 0x00); // Duration = 1LSBs * (1/10Hz) = 0.1s.
+  Sensor.writeRegister(LIS3DH_INT1_CFG, (LIS3DHEnums::INT_CFG::XHIE | LIS3DHEnums::INT_CFG::YHIE));
+  Sensor.writeRegister(LIS3DH_INT2_THS, 0x10);      // Threshold (THS) = 0001 0000 = 16 LSBs * 15.625mg/LSB = 250mg.
+  Sensor.writeRegister(LIS3DH_INT2_DURATION, 0x00); // Duration = 1LSBs * (1/10Hz) = 0.1s.
+  Sensor.writeRegister(LIS3DH_INT2_CFG, (LIS3DHEnums::INT_CFG::XHIE | LIS3DHEnums::INT_CFG::YHIE));
+  Sensor.writeRegister(LIS3DH_CTRL_REG5, (LIS3DHEnums::CTRL_REG5::LIR_INT1 | LIS3DHEnums::CTRL_REG5::LIR_INT2));
 
   Serial.println("Wrote all the registers");
 
   pinMode(WB_IO5, INPUT);
-  attachInterrupt(WB_IO5, pin_high, RISING);
-  Serial.println("Interrupt is setup");
+  attachInterrupt(WB_IO5, int1_trigger, RISING);
+  Serial.println("Interrupt 1 is setup");
+
+  pinMode(WB_IO6, INPUT);
+  attachInterrupt(WB_IO6, int2_trigger, RISING);
+  Serial.println("Interrupt 2 is setup");
 }
 
 void loop()
 {
-  Serial.println("======");
+
   uint8_t data;
-  Sensor.readRegister(&data, LIS3DH_INT1_SRC);
-  Serial.printf("INT SOURC : %d\r\n", data);
   lis3dh_read_data();
-  delay(10000);
-  if (g_moved == true)
+  delay(500);
+  if (g_int1_triggered == true)
   {
-    Serial.println("We moved a bit!");
-    g_moved = false;
+    // Serial.println("Interrupt 1 fired!");
+    Sensor.readRegister(&data, LIS3DH_INT1_SRC);
+    Serial.print("INT1_SRC :");
+    describe_src_int(data);
+
+    g_int1_triggered = false;
+  }
+  if (g_int2_triggered == true)
+  {
+    // Serial.println("Interrupt 2 fired!");
+    Sensor.readRegister(&data, LIS3DH_INT2_SRC);
+    Serial.print("INT2_SRC :");
+    describe_src_int(data);
+
+    g_int2_triggered = false;
   }
 }
